c27c8b34d809d67e5e47b6ebb65005f65d49b7eb
abdikadirjerry <abdikadirjerry@gmail.com>
2026-05-06 21:34:05 +0300

    feat(objects/examples): add easy-level object basics examples

---
+++ javascript/objects/examples/easy/access-properties.js
@@ -0,0 +1,7 @@
+const user = { name: "Najio", age: 20 };
+
+// Accessing using dot notation
+console.log("Name:", user.name);
+
+// Accessing using bracket notation
+console.log("Age:", user["age"]);
---
+++ javascript/objects/examples/easy/create-object.js
@@ -0,0 +1,7 @@
+// Simple object literal syntax
+const user = {
+  name: "Najio",
+  age: 20,
+};
+
+console.log("User Object:", user);
