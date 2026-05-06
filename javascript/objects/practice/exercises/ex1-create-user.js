2109a658fd74b1484a898562d35d9830ac54b6eb
abdikadirjerry <abdikadirjerry@gmail.com>
2026-05-06 21:37:32 +0300

    test(objects): add exercise for creating object

---
+++ javascript/objects/practice/exercises/ex1-create-user.js
@@ -0,0 +1,20 @@
+/**
+ * EXERCISE 1: Create an Object
+ * ❓ Create an object named 'profile' with:
+ * - name (string)
+ * - age (number)
+ * - city (string)
+ */
+
+/**
+ * EXERCISE 1: Create an Object
+ * Task: Create an object named 'profile' with name, age, and city.
+ */
+
+const profile = {
+  name: "Najio",
+  age: 20,
+  city: "Hargeisa",
+};
+
+console.log("Profile created:", profile);
