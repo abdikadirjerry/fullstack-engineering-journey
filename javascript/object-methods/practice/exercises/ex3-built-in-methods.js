e2a6fa4276a29d0d7f3e71ee9c44304823558d42
abdikadirjerry <abdikadirjerry@gmail.com>
2026-05-07 22:57:23 +0300

    test(object-methods): add exercise for built-in object methods

---
+++ javascript/object-methods/practice/exercises/ex3-built-in-methods.js
@@ -0,0 +1,6 @@
+// ❓ Use Object.keys(), values(), and entries()
+const stats = { hp: 100, mp: 50, level: 5 };
+
+console.log(Object.keys(stats));
+console.log(Object.values(stats));
+console.log(Object.entries(stats));
