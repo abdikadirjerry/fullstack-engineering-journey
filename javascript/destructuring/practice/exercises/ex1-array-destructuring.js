d55cf1aea9edb97387ecd642531b3943ea8e76f4
abdikadirjerry <abdikadirjerry@gmail.com>
2026-05-08 23:42:00 +0300

    test(destructuring): add array destructuring exercise

---
+++ javascript/destructuring/practice/exercises/ex1-array-destructuring.js
@@ -0,0 +1,6 @@
+// ❓ Destructure array values into variables
+const scores = [95, 88, 72];
+
+const [math, science, history] = scores;
+
+console.log(`Math: ${math}, Science: ${science}, History: ${history}`);
