47467f309b7640f9f1dd0315c54ef0b43e5cf884
abdikadirjerry <abdikadirjerry@gmail.com>
2026-05-09 21:50:33 +0300

    feat(spread-rest/examples): add easy spread operator examples

---
+++ javascript/spread-rest/examples/easy/merge-arrays.js
@@ -0,0 +1,7 @@
+const frontend = ["HTML", "CSS"];
+const backend = ["Node.js"];
+
+// Merging arrays into a new 'stack' array
+const stack = [...frontend, ...backend];
+
+console.log("Full Stack:", stack);
