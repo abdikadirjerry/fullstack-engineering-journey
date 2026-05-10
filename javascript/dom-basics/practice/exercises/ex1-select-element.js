ca1616594c425efe3ab4548c618667ec601ce953
abdikadirjerry <abdikadirjerry@gmail.com>
2026-05-10 20:53:06 +0300

    test(dom-basics): add DOM element selection exercise

---
+++ javascript/dom-basics/practice/exercises/ex1-select-element.js
@@ -0,0 +1,9 @@
+/* Expected HTML:
+  <h1 id="main-heading">Hello World</h1>
+*/
+
+// ❓ Select element using getElementById
+const heading = document.getElementById("main-heading");
+
+// Verify it was selected
+console.log("Selected element:", heading);
