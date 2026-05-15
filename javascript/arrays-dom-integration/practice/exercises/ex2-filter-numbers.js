a865b593f4abecb74ee077f6e486f2b4015ac3cb
abdikadirjerry <abdikadirjerry@gmail.com>
2026-05-15 22:51:59 +0300

    feat(arrays-dom-integration/exercises): improve number filtering with odd and even separation

--- javascript/arrays-dom-integration/practice/exercises/ex2-filter-numbers.js
+++ javascript/arrays-dom-integration/practice/exercises/ex2-filter-numbers.js
@@ -1,12 +1,20 @@
 // ======================================
 // EXERCISE 2
-// FILTER NUMBERS
+// FILTER EVEN & ODD NUMBERS
 // ======================================
 
 // Numbers array
-const numbers = [10, 15, 20, 25, 30];
+const numbers = [10, 15, 20, 25, 30, 35];
 
 // Filter even numbers
 const evenNumbers = numbers.filter((number) => number % 2 === 0);
 
+// Filter odd numbers
+const oddNumbers = numbers.filter((number) => number % 2 !== 0);
+
+// Output results
+console.log("Even Numbers:");
 console.log(evenNumbers);
+
+console.log("Odd Numbers:");
+console.log(oddNumbers);
