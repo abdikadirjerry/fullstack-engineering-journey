eeb3aac29321a00f4a729dd20541bb2ad59d40c6
abdikadirjerry <abdikadirjerry@gmail.com>
2026-05-16 19:46:32 +0300

    test(localstorage/exercises): add exercise for removing localStorage item

---
+++ javascript/localstorage/practice/exercises/ex2-remove-item.js
@@ -0,0 +1,12 @@
+// ======================================
+// EXERCISE 2
+// REMOVE ITEM
+// ======================================
+
+// Save item
+localStorage.setItem("course", "JavaScript");
+
+// Remove item
+localStorage.removeItem("course");
+
+console.log("Item removed");
