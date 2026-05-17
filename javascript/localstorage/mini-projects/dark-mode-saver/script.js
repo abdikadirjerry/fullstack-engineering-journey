eff3fe26fb7b5d0a68293e06c852fe554146c8f3
abdikadirjerry <abdikadirjerry@gmail.com>
2026-05-17 19:01:23 +0300

    feat(localstorage/project): improve dark mode saver with dynamic status title

--- javascript/localstorage/mini-projects/dark-mode-saver/script.js
+++ javascript/localstorage/mini-projects/dark-mode-saver/script.js
@@ -2,8 +2,10 @@
 // DARK MODE SAVER
 // ======================================
 
-// DOM button
+// DOM elements
 const button = document.getElementById("theme-btn");
+
+const title = document.querySelector("h1");
 
 // Load saved theme
 const darkMode = JSON.parse(localStorage.getItem("darkMode"));
@@ -11,14 +13,20 @@ const darkMode = JSON.parse(localStorage.getItem("darkMode"));
 // Apply saved theme
 if (darkMode) {
   document.body.classList.add("dark-mode");
+
+  title.innerText = "Dark Mode Enabled";
 }
 
 // Toggle theme
 button.addEventListener("click", () => {
   document.body.classList.toggle("dark-mode");
 
-  // Save theme state
+  // Check mode
   const isDarkMode = document.body.classList.contains("dark-mode");
 
+  // Update title
+  title.innerText = isDarkMode ? "Dark Mode Enabled" : "Dark Mode Disabled";
+
+  // Save mode
   localStorage.setItem("darkMode", JSON.stringify(isDarkMode));
 });
