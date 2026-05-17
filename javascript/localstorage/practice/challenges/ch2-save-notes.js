4f33ffff249c00ed852cb84ef5578e5fedff412d
abdikadirjerry <abdikadirjerry@gmail.com>
2026-05-17 19:00:12 +0300

    feat(localstorage/challenges): improve notes challenge with save status message

--- javascript/localstorage/practice/challenges/ch2-save-notes.js
+++ javascript/localstorage/practice/challenges/ch2-save-notes.js
@@ -1,15 +1,20 @@
 // ======================================
 // CHALLENGE 2
-// SAVE NOTES
+// AUTO SAVE NOTES WITH STATUS
 // ======================================
 
 // DOM elements
 const textarea = document.getElementById("notes");
 
+const status = document.getElementById("status");
+
 // Load saved notes
 textarea.value = localStorage.getItem("notes") || "";
 
-// Save notes while typing
+// Save notes
 textarea.addEventListener("input", () => {
   localStorage.setItem("notes", textarea.value);
+
+  // Update status
+  status.innerText = "Notes saved successfully";
 });
