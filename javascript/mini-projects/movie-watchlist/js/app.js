17266e496aeecdd8e1c550952c9cd16ea3094424
abdikadirjerry <abdikadirjerry@gmail.com>
2026-05-19 20:06:55 +0300

    feat: implement add movie functionality

--- javascript/mini-projects/movie-watchlist/js/app.js
+++ javascript/mini-projects/movie-watchlist/js/app.js
@@ -1,12 +1,25 @@
 import { renderMovies } from "./ui.js";
 
+const form = document.getElementById("movie-form");
+const titleInput = document.getElementById("movie-title");
+const statusInput = document.getElementById("movie-status");
 const movieList = document.getElementById("movie-list");
 
-const movies = [
-  {
-    title: "Interstellar",
-    status: "Watching",
-  },
-];
+const movies = [];
 
 renderMovies(movies, movieList);
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const movie = {
+    title: titleInput.value,
+    status: statusInput.value,
+  };
+
+  movies.push(movie);
+
+  renderMovies(movies, movieList);
+
+  form.reset();
+});
