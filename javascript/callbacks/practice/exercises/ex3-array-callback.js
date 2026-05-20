55dc18cd4ea9226b5d7afd70b19a0137d5739e6b
abdikadirjerry <abdikadirjerry@gmail.com>
2026-05-20 20:45:59 +0300

    test(callbacks/exercises): add array callback exercise

---
+++ javascript/callbacks/practice/exercises/ex3-array-callback.js
@@ -0,0 +1,12 @@
+// ======================================
+// EXERCISE 3
+// ARRAY CALLBACK
+// ======================================
+
+// Array
+const fruits = ["Apple", "Banana", "Orange"];
+
+// Loop using callback
+fruits.forEach((fruit) => {
+  console.log(fruit);
+});
