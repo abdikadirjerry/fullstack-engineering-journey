3363b44909ee715fcceb56eb6cd227beb5f17be6
abdikadirjerry <abdikadirjerry@gmail.com>
2026-05-21 21:43:42 +0300

    feat(async-basics/challenges): build async order processing challenge

---
+++ javascript/async-basics/practice/challenges/ch2-order-processing.js
@@ -0,0 +1,13 @@
+// ======================================
+// CHALLENGE 2
+// ORDER PROCESSING
+// ======================================
+
+console.log("Order received");
+
+// Simulate processing
+setTimeout(() => {
+  console.log("Order processed successfully");
+}, 4000);
+
+console.log("Processing started...");
