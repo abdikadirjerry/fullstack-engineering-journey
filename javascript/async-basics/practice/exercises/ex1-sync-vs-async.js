fea7a6d3ae492ee68ea6617c87058fb21a07f35e
abdikadirjerry <abdikadirjerry@gmail.com>
2026-05-21 21:42:49 +0300

    test(async-basics/exercises): add sync vs async execution exercise

---
+++ javascript/async-basics/practice/exercises/ex1-sync-vs-async.js
@@ -0,0 +1,12 @@
+// ======================================
+// EXERCISE 1
+// SYNC VS ASYNC
+// ======================================
+
+console.log("First");
+
+setTimeout(() => {
+  console.log("Second");
+}, 2000);
+
+console.log("Third");
