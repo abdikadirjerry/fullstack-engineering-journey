3543af735777abcf0fa2876ad248e72dbfa909d2
abdikadirjerry <abdikadirjerry@gmail.com>
2026-05-21 21:43:29 +0300

    feat(async-basics/challenges): build loading system async challenge

---
+++ javascript/async-basics/practice/challenges/ch1-loading-system.js
@@ -0,0 +1,10 @@
+// ======================================
+// CHALLENGE 1
+// LOADING SYSTEM
+// ======================================
+
+console.log("Loading application...");
+
+setTimeout(() => {
+  console.log("Application loaded successfully");
+}, 3000);
