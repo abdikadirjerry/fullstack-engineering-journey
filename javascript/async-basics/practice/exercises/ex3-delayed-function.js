a145f625e45e208892a26c527ced3df13c73f2eb
abdikadirjerry <abdikadirjerry@gmail.com>
2026-05-21 21:43:16 +0300

    test(async-basics/exercises): add delayed function execution exercise

---
+++ javascript/async-basics/practice/exercises/ex3-delayed-function.js
@@ -0,0 +1,12 @@
+// ======================================
+// EXERCISE 3
+// DELAYED FUNCTION
+// ======================================
+
+// Function
+function loginUser() {
+  console.log("User logged in");
+}
+
+// Delay login
+setTimeout(loginUser, 2000);
