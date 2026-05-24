8f6dcda0d3811c322c2bb8989c407cf29a841250
abdikadirjerry <abdikadirjerry@gmail.com>
2026-05-24 21:11:51 +0300

    test(async-await-basics/exercises): add basic async function exercise

---
+++ javascript/async-await-basics/practice/exercises/ex1-basic-async.js
@@ -0,0 +1,12 @@
+// ======================================
+// EXERCISE 1
+// BASIC ASYNC
+// ======================================
+
+async function greetUser() {
+  return "Hello Ahmed";
+}
+
+greetUser().then((result) => {
+  console.log(result);
+});
