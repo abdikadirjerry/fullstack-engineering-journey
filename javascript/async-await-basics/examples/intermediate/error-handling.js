6129d4a00783bf04f86503ab502f8affc423428b
abdikadirjerry <abdikadirjerry@gmail.com>
2026-05-25 22:07:10 +0300

    refactor(async-await/examples): improve async error handling example

--- javascript/async-await-basics/examples/intermediate/error-handling.js
+++ javascript/async-await-basics/examples/intermediate/error-handling.js
@@ -7,12 +7,18 @@ function fetchData() {
 }
 
 async function loadData() {
+  console.log("Starting request...");
+
   try {
     const result = await fetchData();
 
     console.log(result);
   } catch (error) {
+    console.log("Error occurred:");
+
     console.log(error);
+  } finally {
+    console.log("Request finished");
   }
 }
 
