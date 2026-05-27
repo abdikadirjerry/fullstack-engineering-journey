a4cd191b15b9dee2f349e930250251ad472b20f2
abdikadirjerry <abdikadirjerry@gmail.com>
2026-05-28 00:26:08 +0300

    feat(async-fetch/examples): improve github user fetch example

--- javascript/async-await-fetch-api/examples/advanced/github-user-fetch.js
+++ javascript/async-await-fetch-api/examples/advanced/github-user-fetch.js
@@ -4,11 +4,19 @@
 
 async function getGitHubUser() {
   try {
+    console.log("Fetching GitHub user...");
+
     const response = await fetch("https://api.github.com/users/octocat");
 
     const user = await response.json();
 
+    console.log("GitHub user loaded");
+
     console.log(user);
+
+    console.log(`Username: ${user.login}`);
+
+    console.log(`Followers: ${user.followers}`);
   } catch (error) {
     console.log("GitHub API Error");
 
