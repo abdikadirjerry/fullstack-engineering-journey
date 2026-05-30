5cff8087b47438e89e853393592e0ae4650f3a06
abdikadirjerry <abdikadirjerry@gmail.com>
2026-05-30 22:53:27 +0300

    feat(api-dom/project): display country capital information

--- javascript/api-dom-integration/mini-projects/country-info-app/script.js
+++ javascript/api-dom-integration/mini-projects/country-info-app/script.js
@@ -7,14 +7,41 @@ const button = document.getElementById("country-btn");
 const countryContainer = document.getElementById("country");
 
 async function loadCountry() {
-  const response = await fetch("https://restcountries.com/v3.1/name/somalia");
+  countryContainer.innerHTML = "<p>Loading country...</p>";
 
-  const country = await response.json();
+  try {
+    const response = await fetch("https://restcountries.com/v3.1/name/somalia");
 
-  countryContainer.innerHTML = `
-    <h2>${country[0].name.common}</h2>
-    <p>Population: ${country[0].population}</p>
-  `;
+    const country = await response.json();
+
+    countryContainer.innerHTML = `
+      <h2>
+        ${country[0].name.common}
+      </h2>
+
+      <p>
+        Population:
+        ${country[0].population}
+      </p>
+
+      <p>
+        Capital:
+        ${country[0].capital[0]}
+      </p>
+
+      <p>
+        Region:
+        ${country[0].region}
+      </p>
+
+      <img
+        src="${country[0].flags.png}"
+        width="150"
+      >
+    `;
+  } catch (error) {
+    countryContainer.innerHTML = "<p>Failed to load country data.</p>";
+  }
 }
 
 button.addEventListener("click", loadCountry);
