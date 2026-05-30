ca60d43e2af724acbcb1f20127011c0b100689d3
abdikadirjerry <abdikadirjerry@gmail.com>
2026-05-30 22:47:32 +0300

    feat(api-dom/examples): display product categories

--- javascript/api-dom-integration/examples/advanced/product-renderer.js
+++ javascript/api-dom-integration/examples/advanced/product-renderer.js
@@ -15,6 +15,7 @@ async function loadProducts() {
         <div>
           <h3>${product.title}</h3>
           <p>$${product.price}</p>
+          <p>${product.category}</p>
         </div>
       `;
     });
