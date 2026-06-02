bded566bc349433ae8e241df70c9497324478d46
abdikadirjerry <abdikadirjerry@gmail.com>
2026-06-02 19:19:22 +0300

    feat(devtracker): add dark mode support

--- javascript/mini-projects/finance-tracker/js/theme.js
+++ javascript/mini-projects/finance-tracker/js/theme.js
@@ -1,1 +1,17 @@
-console.log("Theme Module Loaded");
+const themeButton = document.getElementById("theme-toggle");
+
+const savedTheme = localStorage.getItem("theme");
+
+if (savedTheme === "dark") {
+  document.body.classList.add("dark");
+}
+
+themeButton.addEventListener("click", () => {
+  document.body.classList.toggle("dark");
+
+  const currentTheme = document.body.classList.contains("dark")
+    ? "dark"
+    : "light";
+
+  localStorage.setItem("theme", currentTheme);
+});
