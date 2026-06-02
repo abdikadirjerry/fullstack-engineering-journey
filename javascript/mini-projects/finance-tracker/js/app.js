a418cd4e338c863e1f55c190d98a25a199a46d15
abdikadirjerry <abdikadirjerry@gmail.com>
2026-06-02 19:13:33 +0300

    feat(devtracker): implement task management

--- javascript/mini-projects/finance-tracker/js/app.js
+++ javascript/mini-projects/finance-tracker/js/app.js
@@ -1,1 +1,15 @@
-console.log("DevTracker Pro Loaded");
+const taskForm = document.getElementById("task-form");
+
+const taskInput = document.getElementById("task-input");
+
+taskForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const value = taskInput.value.trim();
+
+  if (!value) return;
+
+  addTask(value);
+
+  taskInput.value = "";
+});
--- javascript/mini-projects/finance-tracker/js/tasks.js
+++ javascript/mini-projects/finance-tracker/js/tasks.js
@@ -1,1 +1,71 @@
-console.log("Tasks Module Loaded");
+let tasks = [];
+
+function addTask(text) {
+  tasks.push({
+    id: Date.now(),
+    text,
+    completed: false,
+  });
+
+  renderTasks();
+}
+
+function deleteTask(id) {
+  tasks = tasks.filter((task) => task.id !== id);
+
+  renderTasks();
+}
+
+function toggleTask(id) {
+  tasks = tasks.map((task) => {
+    if (task.id === id) {
+      return {
+        ...task,
+        completed: !task.completed,
+      };
+    }
+
+    return task;
+  });
+
+  renderTasks();
+}
+
+function renderTasks() {
+  const taskList = document.getElementById("task-list");
+
+  taskList.innerHTML = "";
+
+  tasks.forEach((task) => {
+    taskList.innerHTML += `
+      <li>
+
+        <span
+          style="
+            text-decoration:
+            ${task.completed ? "line-through" : "none"};
+          "
+        >
+          ${task.text}
+        </span>
+
+        <button
+          onclick="
+            toggleTask(${task.id})
+          "
+        >
+          Complete
+        </button>
+
+        <button
+          onclick="
+            deleteTask(${task.id})
+          "
+        >
+          Delete
+        </button>
+
+      </li>
+    `;
+  });
+}
