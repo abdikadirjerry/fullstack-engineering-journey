74f91e80fcf3f42f79e67579c12f69aac06f6c5e
abdikadirjerry <abdikadirjerry@gmail.com>
2026-06-03 21:08:45 +0300

    feat(es-modules): add intermediate module examples

---
+++ javascript/es-modules/examples/intermediate/app.js
@@ -0,0 +1,7 @@
+import { username, age, greet } from "./user.js";
+
+console.log(username);
+
+console.log(age);
+
+greet();
---
+++ javascript/es-modules/examples/intermediate/user.js
@@ -0,0 +1,7 @@
+export const username = "Jerry";
+
+export const age = 20;
+
+export function greet() {
+  console.log(`Hello ${username}`);
+}
