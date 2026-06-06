4bf180e16550c9d1cf7a5db4e7d42d48d2b274ab
abdikadirjerry <abdikadirjerry@gmail.com>
2026-06-06 23:56:28 +0300

    feat(arrays): categorize products by price range

--- javascript/arrays/examples/advanced/users-management.js
+++ javascript/arrays/examples/advanced/users-management.js
@@ -1,20 +1,37 @@
-const users = [
+const products = [
   {
     id: 1,
-    name: "Ahmed",
+    name: "Laptop",
+    price: 1200,
   },
 
   {
     id: 2,
-    name: "Ali",
+    name: "Mouse",
+    price: 25,
   },
 
   {
     id: 3,
-    name: "Jerry",
+    name: "Keyboard",
+    price: 80,
+  },
+
+  {
+    id: 4,
+    name: "Monitor",
+    price: 300,
   },
 ];
 
-const foundUser = users.find((user) => user.id === 2);
+const expensiveProducts = products.filter((product) => product.price >= 100);
 
-console.log(foundUser);
+const affordableProducts = products.filter((product) => product.price < 100);
+
+console.log("Expensive Products:");
+
+console.log(expensiveProducts);
+
+console.log("Affordable Products:");
+
+console.log(affordableProducts);
