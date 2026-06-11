08f50f67d42cb30d2fe89b2de4dce206bdd61064
abdikadirjerry <abdikadirjerry@gmail.com>
2026-06-11 23:34:37 +0300

    feat(control-flow): solve exercise 3

---
+++ javascript/control-flow/practice/exercises/ex3.js
@@ -0,0 +1,14 @@
+const role = "user";
+
+switch (role) {
+  case "admin":
+    console.log("Admin");
+    break;
+
+  case "user":
+    console.log("User");
+    break;
+
+  default:
+    console.log("Guest");
+}
