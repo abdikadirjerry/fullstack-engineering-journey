18349288588d73ddb7f849476fdd130868fa2480
abdikadirjerry <abdikadirjerry@gmail.com>
2026-06-11 23:35:58 +0300

    feat(control-flow): complete challenge 1

---
+++ javascript/control-flow/practice/challenges/ch1.js
@@ -0,0 +1,9 @@
+const username = "admin";
+
+const password = "123456";
+
+if (username === "admin" && password === "123456") {
+  console.log("Login Successful");
+} else {
+  console.log("Invalid Credentials");
+}
