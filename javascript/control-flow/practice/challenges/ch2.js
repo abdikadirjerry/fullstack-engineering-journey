2fdbfaeef92d2844dc2b8668d6355e78b207b947
abdikadirjerry <abdikadirjerry@gmail.com>
2026-06-11 23:36:19 +0300

    feat(control-flow): complete challenge 2

---
+++ javascript/control-flow/practice/challenges/ch2.js
@@ -0,0 +1,15 @@
+const score = 78;
+
+let grade;
+
+if (score >= 90) {
+  grade = "A";
+} else if (score >= 80) {
+  grade = "B";
+} else if (score >= 70) {
+  grade = "C";
+} else {
+  grade = "F";
+}
+
+console.log(`Grade: ${grade}`);
