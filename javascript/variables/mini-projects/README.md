5ba3ec77887ec740ad1c9c29761be69c703abde9
abdikadirjerry <abdikadirjerry@gmail.com>
2026-06-12 20:52:12 +0300

    feat(profile-generator): create profile generator project

---
+++ javascript/variables/mini-projects/README.md
@@ -0,0 +1,8 @@
+# Profile Generator
+
+Features:
+
+- Store user data in variables
+- Display profile information
+- Practice const and let
+- String interpolation
---
+++ javascript/variables/mini-projects/profile-generator.js
@@ -0,0 +1,19 @@
+const firstName = "Ahmed";
+
+const lastName = "Ali";
+
+const age = 20;
+
+const country = "Somaliland";
+
+function generateProfile() {
+  console.log("===== PROFILE =====");
+
+  console.log(`Name: ${firstName} ${lastName}`);
+
+  console.log(`Age: ${age}`);
+
+  console.log(`Country: ${country}`);
+}
+
+generateProfile();
