411aa7d2bed29ef2deafff9fb98bb3f26547f20b
abdikadirjerry <abdikadirjerry@gmail.com>
2026-06-13 21:13:58 +0300

    feat(user-access): create scope mini project

---
+++ javascript/scope/mini-projects/user-access-system.js
@@ -0,0 +1,13 @@
+const appName = "Access System";
+
+function login() {
+  const username = "Ahmed";
+
+  function displayUser() {
+    console.log(`${appName} - Welcome ${username}`);
+  }
+
+  displayUser();
+}
+
+login();
