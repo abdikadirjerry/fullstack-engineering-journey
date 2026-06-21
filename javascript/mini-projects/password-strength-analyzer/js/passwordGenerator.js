0f29607f87f507fd197b4494f9a9f5a1def13a0a
abdikadirjerry <abdikadirjerry@gmail.com>
2026-06-21 20:44:32 +0300

    Add password generator

---
+++ javascript/mini-projects/password-strength-analyzer/js/passwordGenerator.js
@@ -0,0 +1,17 @@
+function generatePassword(length = 16) {
+  const characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZ" +
+    "abcdefghijklmnopqrstuvwxyz" +
+    "0123456789" +
+    "!@#$%^&*()_+{}[]<>?/";
+
+  let password = "";
+
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * characters.length);
+
+    password += characters[randomIndex];
+  }
+
+  return password;
+}
