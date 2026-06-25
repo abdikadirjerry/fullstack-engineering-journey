cffd09032e6b5c6e839e06b6b98846f2be143664
abdikadirjerry <abdikadirjerry@gmail.com>
2026-06-25 23:13:06 +0300

    Add status filtering

--- javascript/mini-projects/bug-report-dashboard/js/statistics.js
+++ javascript/mini-projects/bug-report-dashboard/js/statistics.js
@@ -1,25 +1,11 @@
 function updateStatistics(bugs) {
+  document.getElementById("total-bugs").textContent = bugs.length;
 
-    document.getElementById(
-        "total-bugs"
-    ).textContent =
-        bugs.length;
+  document.getElementById("open-bugs").textContent = bugs.filter(
+    (bug) => bug.status === "Open",
+  ).length;
 
-    document.getElementById(
-        "open-bugs"
-    ).textContent =
-        bugs.filter(
-            bug =>
-                bug.status ===
-                "Open"
-        ).length;
-
-    document.getElementById(
-        "fixed-bugs"
-    ).textContent =
-        bugs.filter(
-            bug =>
-                bug.status ===
-                "Fixed"
-        ).length;
+  document.getElementById("fixed-bugs").textContent = bugs.filter(
+    (bug) => bug.status === "Fixed",
+  ).length;
 }
